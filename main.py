b6df1c3f26c405bb10efd560ea7cc76ffb626599
kylejwolff <kylejwolff@outlook.com>
2021-09-15 12:59:49 -0400

    read and print csv done

--- main.py
+++ main.py
@@ -18,9 +18,9 @@ def read_csv(file_path):
         file = open(file_path, "r")
         line = file.readline()
         while line:
-            words = line.split(",")
+            words = line.rstrip().split(",")
             arr2 = []
-            for _i in range(0, len(words)-1):
+            for _i in range(0, len(words)):
                 arr2.append(words[_i])
             arr.append(arr2)
             line = file.readline()
@@ -32,9 +32,34 @@ def read_csv(file_path):
         file.close()
 
 
+def print_table(arr: list, arr_title):
+    print(f"Title: {arr_title}")
+    line = ""
+    for _i in range(0, len(arr[0])):
+        if len(arr[0][_i]) < 6:
+            line = line + "| " + arr[0][_i] + "\t\t"
+        else:
+            line = line + "| " + arr[0][_i] + "\t"
+    print(line)
+    line = ""
+    for _i in range(0, len(arr[0])):
+        line = line + "________________"
+    print(line)
+    for _i in range(1, len(arr)):
+        line = ""
+        for _j in range(0, len(arr[0])):
+            if len(arr[_i][_j]) < 6:
+                line = line + "| " + arr[_i][_j] + "\t\t"
+            else:
+                line = line + "| " + arr[_i][_j] + "\t"
+        print(line)
+
+
 my_client = pymongo.MongoClient("mongodb://localhost:27017/")
 my_db = my_client["mydatabase"]
 
+table = []
+title = ""
 clear()
 run = True
 while run:
@@ -49,10 +74,14 @@ while run:
     user_entry = input("Enter a menu option from the list: ")
     if user_entry is "1":
         clear()
-        print("This will read a csv")
+        path = input("Enter the path of the file: ")
+        title = path
+        table = read_csv(path)
     elif user_entry is "2":
         clear()
-        print("This will print the current table")
+        # for line in table:
+        #     print(line)
+        print_table(table, title)
     elif user_entry is "3":
         clear()
         print("This will push the table to MongoDB")
